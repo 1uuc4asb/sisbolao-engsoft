b2411fd9322cc0a58367ef669c6d2402f103aa10
1uuc4asb <1uuc4asb@gmail.com>
2018-12-16 20:41:32 -0300

    x

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     $server = 'localhost';
     $db_user = 'root';
-    $db_password = 'Lucas@2301';
+    $db_password = 'x';
     $conn = new mysqli($server,$db_user,$db_password, "Bolao");
     // Check connection
     if ($conn->connect_error) {
