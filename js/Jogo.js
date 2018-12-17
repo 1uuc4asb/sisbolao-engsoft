25e9823e13a0ab04ad26b794b8ab5cef6a4475c8
matheusbgs <41394195+matheusbgs@users.noreply.github.com>
2018-12-16 23:01:20 -0300

    Excluido metodo duplicado e realizado atualizacao no UML

--- js/Jogo.js
+++ js/Jogo.js
@@ -78,13 +78,6 @@ class Jogo {
         }
     }
 
-    getApostas() {
-        return this.apostas;
-    }
-
-    setApostas(apostas) {
-        this.apostas = apostas;
-    }
     
     getId() {
         return this.id;
